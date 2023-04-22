5261bd1cea4f19f3799505e24efbfda818c828b6
Vincent Nguyen <67670654+tng296@users.noreply.github.com>
2023-04-22 18:27:55 -0500

    Update createDB.sql

--- createDB.sql
+++ createDB.sql
@@ -2,42 +2,41 @@ create database LibraryDBMS;
 use LibraryDBMS;
 
 CREATE TABLE Branch (
-  branchID INT NOT NULL UNIQUE KEY AUTO_INCREMENT,
+  branchID VARCHAR(4) NOT NULL UNIQUE KEY,
   address VARCHAR(255) NOT NULL,
   openHour TIME NOT NULL,
   closeHour TIME NOT NULL,
   openDay TEXT NOT NULL,
   PRIMARY KEY (branchID)
 );
-
 CREATE TABLE Admin (
-  adminID INT NOT NULL UNIQUE KEY AUTO_INCREMENT,
+  adminID VARCHAR(4) NOT NULL UNIQUE KEY,
   title VARCHAR(50) NOT NULL,
   lName VARCHAR(50) NOT NULL,
   fName VARCHAR(50) NOT NULL,
   age INT NOT NULL,
   sex ENUM('M', 'F') NOT NULL,
   email VARCHAR(255) NOT NULL,
-  branchID INT NOT NULL,
+  branchID VARCHAR(4) NOT NULL,
   PRIMARY KEY (adminID),
   FOREIGN KEY (branchID) REFERENCES Branch(branchID)
 );
-
 CREATE TABLE Staff (
-  staffID INT NOT NULL UNIQUE KEY AUTO_INCREMENT,
+  staffID VARCHAR(4) NOT NULL UNIQUE KEY,
   title VARCHAR(50) NOT NULL,
   lName VARCHAR(50) NOT NULL,
   fName VARCHAR(50) NOT NULL,
   age INT NOT NULL,
   sex ENUM('M', 'F') NOT NULL,
   email VARCHAR(255) NOT NULL,
-  branchID INT NOT NULL,
+  branchID VARCHAR(4)NOT NULL,
+  hiredBy VARCHAR(4)NOT NULL,
   PRIMARY KEY (staffID),
-  FOREIGN KEY (branchID) REFERENCES Branch(branchID)
+  FOREIGN KEY (branchID) REFERENCES Branch(branchID),
+  FOREIGN KEY (hiredBy) REFERENCES Admin(adminID)
 );
-
 CREATE TABLE Member (
-  memberID INT NOT NULL UNIQUE KEY AUTO_INCREMENT,
+  memberID VARCHAR(4) NOT NULL UNIQUE KEY,
   lName VARCHAR(50) NOT NULL,
   fName VARCHAR(50) NOT NULL,
   age INT NOT NULL,
@@ -48,12 +47,11 @@ CREATE TABLE Member (
   hold BOOLEAN NOT NULL,
   PRIMARY KEY (memberID)
 );
-
 CREATE TABLE Book (
   isbn VARCHAR(13) NOT NULL UNIQUE,
   genre VARCHAR(50) NOT NULL,
   title VARCHAR(255) NOT NULL,
-  branchID INT NOT NULL,
+  branchID VARCHAR(4) NOT NULL,
   status ENUM('Available', 'On Loan', 'Lost') NOT NULL,
   publishedBy VARCHAR(255) NOT NULL,
   language VARCHAR(50) NOT NULL,
@@ -61,54 +59,50 @@ CREATE TABLE Book (
   PRIMARY KEY (isbn),
   FOREIGN KEY (branchID) REFERENCES Branch(branchID)
 );
-
 CREATE TABLE Author (
-  authorID INT PRIMARY KEY AUTO_INCREMENT,
+  authorID VARCHAR(4) PRIMARY KEY,
   lName VARCHAR(255) NOT NULL,
   fName VARCHAR(255) NOT NULL
 );
-
 CREATE TABLE WrittenBy (
   isbn VARCHAR(13) NOT NULL,
-  authorID INT NOT NULL,
+  authorID VARCHAR(4) NOT NULL,
   PRIMARY KEY (isbn, authorID),
   FOREIGN KEY (isbn) REFERENCES Book(isbn),
   FOREIGN KEY (authorID) REFERENCES Author(authorID)
 );
-
 CREATE TABLE Request (
-  requestID INT NOT NULL UNIQUE KEY AUTO_INCREMENT,
-  memberID INT NOT NULL,
+  requestID VARCHAR(4) NOT NULL UNIQUE KEY,
+  memberID VARCHAR(4) NOT NULL,
   status ENUM('Pending', 'Approved', 'Denied') NOT NULL,
-  staffID INT NOT NULL,
+  staffID VARCHAR(4) NOT NULL,
   date DATE NOT NULL,
   time TIME NOT NULL,
-  branchID INT NOT NULL,
+  branchID VARCHAR(4) NOT NULL,
   PRIMARY KEY (requestID),
   FOREIGN KEY (memberID) REFERENCES Member(memberID),
   FOREIGN KEY (staffID) REFERENCES Staff(staffID),
   FOREIGN KEY (branchID) REFERENCES Branch(branchID)
 );
-
 CREATE TABLE Register (
   username VARCHAR(255) NOT NULL,
   password VARCHAR(50) NOT NULL,
-  staffID INT ,
-  memberID INT,
+  staffID VARCHAR(4) ,
+  memberID VARCHAR(4),
   PRIMARY KEY (username),
   FOREIGN KEY (staffID) REFERENCES Staff(staffID),
   FOREIGN KEY (memberID) REFERENCES Member(memberID)
 );
-
 CREATE TABLE `Order` (
-  orderID INT NOT NULL UNIQUE KEY AUTO_INCREMENT,
-  requestID INT NOT NULL,
+  orderID VARCHAR(4) NOT NULL UNIQUE KEY,
+  requestID VARCHAR(4) NOT NULL,
+  isbn VARCHAR(13) NOT NULL UNIQUE,
   borrowDate DATE NOT NULL,
   returnDate DATE NOT NULL,
   status ENUM('Pending', 'Active', 'Returned', 'Overdue') NOT NULL,
   dueDate DATE NOT NULL,
   accuredFine DECIMAL(10,2) NOT NULL,
   PRIMARY KEY (orderID),
-  FOREIGN KEY (requestID) REFERENCES Request(requestID)
+  FOREIGN KEY (requestID) REFERENCES Request(requestID),
+  FOREIGN KEY (isbn) REFERENCES Book(isbn)
 );
-
